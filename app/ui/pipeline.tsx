34ec7c229379f67ed124bbec82f3d91434fe7f13
louislam <louislam7229@gmail.com>
2023-11-28 18:59:17 -0800

    chore(data): remove unecessary methods and imports

--- app/ui/pipeline.tsx
+++ app/ui/pipeline.tsx
@@ -1,13 +1,5 @@
 import PipelineStats from "@/app/ui/pipelineStats";
-import {
-  PipelineStatsType,
-  // activePipelineCheck,
-  // getPipelineNames,
-  database,
-  DataType,
-  pipelineStatistics,
-  // streamRecordCheck,
-} from "../lib/data";
+import { pipelineStatistics } from "../lib/data";
 
 export default async function Pipeline() {
   //retrieves the pipeline names from the Database
@@ -17,40 +9,38 @@ export default async function Pipeline() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-col-3 2xl:grid-cols-4 gap-4 md:gap-8">
-      {pipelineStatistics.map(
-        (pipeline: pipelineStatistics): JSX.Element => {
-          const {
-            id,
-            streamName,
-            active,
-            connection,
-            current_bitrate,
-            resolution,
-            avg_fps,
-            isRecording,
-            recordName,
-            recordDuration,
-            recordStart,
-          } = pipeline;
+      {pipelineStatistics.map((pipeline: pipelineStatistics): JSX.Element => {
+        const {
+          id,
+          streamName,
+          active,
+          connection,
+          current_bitrate,
+          resolution,
+          avg_fps,
+          isRecording,
+          recordName,
+          recordDuration,
+          recordStart,
+        } = pipeline;
 
-          return (
-            <PipelineStats
-              key={id}
-              id={id}
-              streamName={streamName}
-              active={active}
-              connection={connection}
-              current_bitrate={current_bitrate}
-              resolution={resolution}
-              avg_fps={avg_fps}
-              isRecording={isRecording}
-              recordName={recordName}
-              recordDuration={recordDuration}
-              recordStart={recordStart}
-            />
-          );
-        }
-      )}
+        return (
+          <PipelineStats
+            key={id}
+            id={id}
+            streamName={streamName}
+            active={active}
+            connection={connection}
+            current_bitrate={current_bitrate}
+            resolution={resolution}
+            avg_fps={avg_fps}
+            isRecording={isRecording}
+            recordName={recordName}
+            recordDuration={recordDuration}
+            recordStart={recordStart}
+          />
+        );
+      })}
     </div>
   );
 }
