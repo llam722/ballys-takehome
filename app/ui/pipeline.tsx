5e84019e5815d4a7a873a66d6b2cacd5b5c80a67
louislam <louislam7229@gmail.com>
2023-11-29 12:09:46 -0800

    feature(pipeline): add limiter on pipeline and revalidation

--- app/ui/pipeline.tsx
+++ app/ui/pipeline.tsx
@@ -3,14 +3,10 @@ import { pipelineStatistics } from "../lib/types";
 import { limiter } from "../lib/utils";
 
 export default async function Pipeline() {
-  //retrieves the pipeline names from the Database
-  const pipeline = await fetch("http:localhost:3000/view")
+  //retrieves the pipeline names from the Database, revalidates every 5 seconds 
+  //30 requests / 2 = 15 active pipelines / 5 requests per second = 3 seconds to send all requests, giving it a buffer of 2 seconds
+  const pipeline = await limiter.schedule(() => fetch("http:localhost:3000/view", {next: {revalidate: 5000}}))
   const pipelineStatistics = await pipeline.json();
-
-  // console.log(pipelineStatistics, 'pipelineStatistics')
-
-  
-
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-col-3 2xl:grid-cols-4 gap-4 md:gap-8">
